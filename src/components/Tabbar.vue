5e12ec7e478ec182930645477f4b3d1495bf9af4
ZHZHAN000 <1125329576@qq.com>
2019-12-17 19:15:43 +0800

    add

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -1,22 +1,12 @@
 <template>
 	<van-tabbar v-model="active">
-<<<<<<< HEAD
-	  <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-	  <van-tabbar-item icon="search">标签</van-tabbar-item>
-	  <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-	  <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
-	</van-tabbar>
-</template>
-
-
-=======
 		<van-tabbar-item icon="wap-home-o" to="/home">首页</van-tabbar-item>
 		<van-tabbar-item icon="qr" to='/classify'>分类</van-tabbar-item>
 		<van-tabbar-item icon="shopping-cart-o" to='/cart'>购物车</van-tabbar-item>
 		<van-tabbar-item icon="manager-o" to='/mine'>个人</van-tabbar-item>
 	</van-tabbar>
 </template>
->>>>>>> zyf/xiaomi
+
 <script type="text/javascript">
 	export default {
 		data() {
