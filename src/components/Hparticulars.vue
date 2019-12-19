b13b541bfe4caafff548833cc7766fd7250270fa
baobaoyuzhou <584606916@qq.com>
2019-12-19 16:13:38 +0800

    首页详情购物车ok

--- src/components/Hparticulars.vue
+++ src/components/Hparticulars.vue
@@ -18,11 +18,10 @@
             <p>{{item.info}}</p>
             <span>￥{{item.price}}</span>
             <div>
-                <div class="top" @click="show = true">
+                <div class="top" @click="addobj">
                     <div class="zuo">已选</div>
                     <div class="zhong" >
-                        <p>{{item.name}}</p>
-                        <p v-for="item in item.edition" :key="item.id">{{item.Memory}}</p>
+                        <p>{{content.name }} {{ content.edition[sub].Memory}} {{ content.edition[sub].color[sub1].color_list }}</p>
                     </div>
                     <div class="you">
                         <van-icon name="arrow" size="0.3rem"/>
@@ -43,15 +42,40 @@
         <van-goods-action>
             <van-goods-action-icon icon="wap-home-o" text="首页" @click="fn1"/>
             <van-goods-action-icon icon="cart-o" text="购物车" info="5" @click="fn2"/>
-            <van-goods-action-button type="warning" text="加入购物车" size="2.56rem"  @click="show = true"/>
+            <van-goods-action-button type="warning" text="加入购物车" size="2.56rem"  @click="addobj"/>
             <van-goods-action-button type="danger" text="立即购买" />
         </van-goods-action>
-        <!-- 弹出页面 -->
-        <!-- 蒙版 -->
+        <!-- 弹出页面蒙版 -->
         <div class="meng" v-show="show">
+            <!-- 加入购物车 -->
             <div class="bai">
                 <p @click="show = false">X</p>
-                <img src="" alt="">
+                <div class="da">
+                    <div class="shang">
+                        <!-- <img src="">
+                        <span>￥</span>
+                        <p></p> -->
+                        <img :src=" content.edition[sub].color[sub1].img ">
+                        <span>￥{{content.edition[sub].edition_price}}</span>
+                        <p>{{content.name }} {{ content.edition[sub].Memory}} {{ content.edition[sub].color[sub1].color_list }}</p>
+                    </div>
+                    <div class="zj">
+                        <p>版本</p>
+                        <div v-for="(item,i) in content.edition" :key="item.id" @click="color(i)" :class="{act:sub==i}">
+                            <p>{{ item.Memory}}</p>
+                            <span>￥{{ item.edition_price }}</span>
+                        </div>
+                    </div>
+                    <div class="xia">
+                        <p>颜色</p>
+                        <div v-for="(item,i) in content2.color" :key="item.id" @click="color2(i)" :class="{active:sub1==i}">{{ item.color_list }}</div>
+                    </div>
+                </div>
+                <div class="mai">
+                    <p>购买数量</p>
+                    <van-stepper v-model="value" />
+                </div>
+                <div class="jia">加入购物车</div>
             </div>
         </div>
       </div> 
@@ -63,7 +87,16 @@ export default {
         return{
             item:this.$route.query.item,
             images:this.$route.query.item.imgList,
+            // 蒙版显示隐藏
             show: false,
+            // 步进器
+            value: 1,
+            content:{},
+            content2:{},
+            sub:0,
+            sub1:0,
+            // colorFlag:0,
+            // colorFlag2:0
         } 
     },
     methods:{
@@ -79,9 +112,26 @@ export default {
         fn2(){
             this.$router.push('/cart')
         },
+        // 点击变颜色
+        color(i){
+            this.sub=i
+            this.content2=this.content.edition[i]
+        },
+        color2(i){
+            this.sub1=i
+        },
+        addobj(){
+            this.content=this.item
+            // console.log(this.content)
+            this.show = true
+        }
     },
     mounted(){
-        console.log(this.item)
+        // addobj()
+        this.content=this.item
+        this.content2=this.content.edition[this.sub]
+        // console.log(this.content.edition[this.sub])
+
     }
 }
 </script>
@@ -201,5 +251,111 @@ export default {
         position: absolute;
         right:0.29rem;
         top:0.31rem;
+    }
+    .da{
+        width:100%;
+        height:7rem;
+    }
+    .shang{
+        width:100%;
+        height:3.05rem;
+
+    }
+    .shang>img{
+        width:1.67rem;
+        height:1.67rem;
+        margin: 0.89rem 0.14rem  0.45rem 0.29rem;
+        border:0.02rem solid #CCCCCC;
+        float: left;
+    }
+    .shang>span{
+        font-size:0.33rem;
+        margin-top:1.33rem;
+        display: inline-block;
+    }
+    .shang>p{
+        font-size: 0.26rem;
+        margin-top:0.23rem;
+    }
+    .zj{
+        width:100%;
+        height:2.11rem;
+    }
+    .zj>p{
+        font-size: 0.2rem;
+        margin-left: 0.27rem;
+
+    }
+    .zj>div{
+        width:5.82rem;
+        height:0.58rem;
+        margin:auto;
+        border:0.02rem solid #ccc;
+        margin-top:0.28rem;
+    }
+    .xia .active,.zj .act{
+        border:0.02rem solid #F56600;
+        color:#F56600;
+    }
+    .zj .act p,.zj .act span{
+        color:#F56600
+    }
+    .zj>div>p{
+        font-size: 0.2rem;
+        color:#ccc;
+        margin-left: 0.28rem;
+        line-height: 0.58rem;
+        float: left;
+    }
+    .zj>div>span{
+        color:#ccc;
+        float: right;
+        font-size: 0.23rem; 
+        line-height: 0.58rem;
+        margin-right: 0.27rem;
+    }
+    .xia{
+        width:100%;
+        height:1.82rem;
+    }
+    .xia>p{
+        margin-left:0.19rem;
+        font-size: 0.2rem;
+    }
+    .xia>div{
+        margin:0.38rem 0 0 0.5rem ;
+        width:1.07rem;
+        height:0.57rem;
+        border: 0.01rem solid #CCCCCC;
+        text-align: center;
+        line-height: 0.57rem;
+        font-size: 0.26rem;
+        display: inline-block;
+        color:#CCCCCC;
+    }
+    .bai>.mai{
+        width: 100%;
+        height:0.91rem;
+    }
+    .mai>p{
+        float: left;
+        margin:0.25rem 0 0 0.27rem;
+    }
+    .mai>.van-stepper{
+        float: right;
+        margin-right: 0.28rem;
+        border:0.02rem solid #BBBBBB;
+        border-radius: 0.05rem;
+    }
+    .bai>.jia{
+        width:5.86rem;
+        height:0.55rem;
+        margin:auto;
+        background: #FF6700;
+        border-radius: 0.5rem;
+        color:#fff;
+        line-height: 0.55rem;
+        text-align: center;
+        font-size: 0.23rem;
     }
 </style>
